5b8f58a6c0de9b92373f10f3a7f761a3d275edc3
malorubiuz <malorubiuz@gmail.com>
2021-05-26 15:21:07 +0200

    -

--- include/asd_progetto2021/common.hpp
+++ include/asd_progetto2021/common.hpp
@@ -1,1 +1,12 @@
 #pragma once
+#include <stdexcept>
+
+#define ASSERT(...)                                                                                          \
+  do {                                                                                                       \
+    if (!(__VA_ARGS__)) {                                                                                    \
+      fprintf (stderr, "ASSERTION FAILED: %s\n", #__VA_ARGS__);                                              \
+      fprintf (stderr, "LINE: %d FILE: %s\n", __LINE__, __FILE__);                                           \
+      fprintf (stderr, "FUNCTION: %s\n", __FUNCTION__);                                                      \
+      std::terminate ();                                                                                     \
+    }                                                                                                        \
+  } while (0)
--- superbuild/superbuild.cpp
+++ superbuild/superbuild.cpp
@@ -1,5 +1,16 @@
+#include <stdexcept>
+#define ASSERT(...)                                                                                          \
+  do {                                                                                                       \
+    if (!(__VA_ARGS__)) {                                                                                    \
+      fprintf (stderr, "ASSERTION FAILED: %s\n", #__VA_ARGS__);                                              \
+      fprintf (stderr, "LINE: %d FILE: %s\n", __LINE__, __FILE__);                                           \
+      fprintf (stderr, "FUNCTION: %s\n", __FUNCTION__);                                                      \
+      std::terminate ();                                                                                     \
+    }                                                                                                        \
+  } while (0)
 #include <iostream>
 int main ()
 {
   std::cout << "Hello world!\n";
+  ASSERT (false);
 }
